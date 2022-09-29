6e968f3689c52d2c84b00a5371c61c4862d22900
gaderman <gaderman244@gmail.com>
2022-09-29 09:26:41 +0200

    Createt tests with 'HalloJavaMitForEach'

--- src/net/htlgkr/gattermannk190189/HalloJavaMitForEach.java
+++ src/net/htlgkr/gattermannk190189/HalloJavaMitForEach.java
@@ -1,7 +1,23 @@
 package net.htlgkr.gattermannk190189;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HalloJavaMitForEach {
 
-    
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add(" Java");
+        list.add(" with");
+        list.add(" ForEach");
+
+        for(String s : list)
+            System.out.println(s);
+
+        list.forEach(s -> System.out.println(s));
+        list.forEach(System.out::println);
+    }
 
 }
