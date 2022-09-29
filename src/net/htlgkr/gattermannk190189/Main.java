594c3938c6f4903627bb274f65619ec2ed1e06d6
gaderman <gaderman244@gmail.com>
2022-09-29 10:17:02 +0200

    NumberTester everything is working. Test in main() are running correctly

--- src/net/htlgkr/gattermannk190189/Main.java
+++ src/net/htlgkr/gattermannk190189/Main.java
@@ -2,6 +2,33 @@ package net.htlgkr.gattermannk190189;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NumberTester numberTester = new NumberTester("C:\\Users\\user\\Documents\\Schule\\3. Klasse\\POS\\NumberTester\\src\\numbers.txt");
+        numberTester.setOddEvenTester(x -> x % 2 == 0);
+        numberTester.setPrimeTester(x -> {
+
+            boolean[] var = new boolean[x + 1];
+
+            for(int i = 2; i <= Math.sqrt(x); i++){
+                if(!var[i]){
+                    for(int j = i*i; j <= x; j += i)
+                        var[j] = true;
+                }
+
+                if(var[x])
+                    return false;
+
+            }
+
+            return true;
+        });
+
+        numberTester.setPalindromeTester(x -> {
+            StringBuilder s = new StringBuilder(x);
+            s.reverse();
+            StringBuilder s1 = new StringBuilder(x);
+            return s.compareTo(new StringBuilder(x)) == 0;
+        });
+
+        numberTester.testFile();
     }
 }
--- src/net/htlgkr/gattermannk190189/NumberTester.java
+++ src/net/htlgkr/gattermannk190189/NumberTester.java
@@ -1,10 +1,14 @@
 package net.htlgkr.gattermannk190189;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester {
     private String fileName;
     private NumberTest oddTester;
     private NumberTest primeTester;
-    private NumberTest palindroneTester;
+    private NumberTest palindromeTester;
 
     public NumberTester(String fileName){
         this.fileName = fileName;
@@ -18,12 +22,37 @@ public class NumberTester {
         this.primeTester = primeTester;
     }
 
-    public void setPalindroneTester(NumberTest palindroneTester){
-        this.palindroneTester = palindroneTester;
+    public void setPalindromeTester(NumberTest palindromeTester){
+        this.palindromeTester = palindromeTester;
     }
 
     public void testFile(){
-        //TODO
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            int cases = Integer.parseInt(br.readLine());
+
+            for(int i = 0; i < cases; i++){
+                String[] parts = br.readLine().split(" ");
+
+                switch(Integer.parseInt(parts[0])){
+                    case 1:
+                       if(oddTester.testNumber(Integer.parseInt(parts[1]))) System.out.println("EVEN");
+                       else System.out.println("ODD");
+                       break;
+
+                    case 2:
+                        if(primeTester.testNumber(Integer.parseInt(parts[1]))) System.out.println("PRIME");
+                        else System.out.println("NO PRIME");
+                        break;
+
+                    case 3:
+                        if(palindromeTester.testNumber(Integer.parseInt(parts[1]))) System.out.println("PALINDROME");
+                        else System.out.println("NO PALINDROME");
+                        break;
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
